53591b8cd3210c07736666db3bcc896a4b7c8e34
119kanika <kanika.mahindroo@gmail.com>
2024-04-10 12:55:26 +0530

    hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/1 antialiased bg-grid-white/[0.02]">
-      <h1>kanika</h1>
+      <HeroSection/>
     </main>
   );
 }
