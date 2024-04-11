ff6e4c58782c5f1ed9f3d0df92a83004d8c8fe69
119kanika <kanika.mahindroo@gmail.com>
2024-04-11 14:33:00 +0530

    css changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,14 +10,14 @@ function HeroSection() {
     >
         <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="purple"
+        fill="blue"
       />
         <div className="p-4 relative z-10 w-full text-center">
 
-            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 dark:text-white">
                 Master The art of Music
             </h1>
-            <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+            <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto dark:text-white">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae incidunt consectetur libero esse? Consequatur assumenda iusto repudiandae tenetur libero.
             </p>
 
